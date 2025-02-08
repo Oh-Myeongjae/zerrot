61d003cb91a17b2c5d939139a312f958ba8cbf05
Oh-Myeongjae <omjcws@gmail.com>
2025-02-09 06:05:03 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/zerrot/backend/ZerrotApplication.java
@@ -0,0 +1,13 @@
+package com.zerrot.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerrotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerrotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerrot/backend/ZerrotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerrot.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerrotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
